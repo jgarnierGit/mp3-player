21ce079cddd05542e9b19f5b3cc164828136dda5
Jonathan G <jonat.garnier@free.fr>
2023-01-28 15:09:58 +0100

    fix: removed poc api exposition

--- audio-player/src/lib.rs
+++ audio-player/src/lib.rs
@@ -2,7 +2,6 @@ mod audio_parser;
 mod symphonia_wrapper;
 pub use audio_parser::metadata_parser_builder as MetadataParserBuilder;
 pub use audio_parser::MetadataParserWrapper;
-pub mod metadata_wrapper;
 pub use symphonia_wrapper::playTrack;
 pub mod audio_tags;
 pub use audio_parser::TagsResult;
